46ff87f460dde9509180c617c5879fe42da9aa85
Jon <jmcreasman89@gmail.com>
2015-09-26 17:13:59 -0400

    edited

--- js/controllers.js
+++ js/controllers.js
@@ -1,19 +1,29 @@
-$("#rewind").click(function() {
-  console.log("rewind")
-});
-
-$("#step_back").click(function() {
-  console.log("stepback")
-});
-
+// $("#rewind").click(function() {
+//   console.log("rewind")
+// });
+//
+// $("#step_back").click(function() {
+//   console.log("stepback")
+// });
+//
+// $("#play_pause").click(function() {
+//   console.log("play/pause")
+// });
+//
+// $("#step_foward").click(function() {
+//   console.log("stepfoward")
+// });
+//
+// $("#fast_foward").click(function() {
+//   console.log("fastfoward")
+// });
+// var moves;
+// moves = ['#start'];
 $("#play_pause").click(function() {
-  console.log("play/pause")
+  $('#start').removeClass('start')
+  $('#white1').addClass('white1')
 });
-
-$("#step_foward").click(function() {
-  console.log("stepfoward")
-});
-
-$("#fast_foward").click(function() {
-  console.log("fastfoward")
+$("#white_1").click(function() {
+  $('#start').addClass('start')
+  $('#white1').removeClass('white1')
 });
