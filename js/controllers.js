b01faa8952dd99c22d5df76b1beca546c2655dbc
Jon <jmcreasman89@gmail.com>
2015-09-27 13:58:50 -0400

    edits to make it work

--- js/controllers.js
+++ js/controllers.js
@@ -17,13 +17,49 @@
 // $("#fast_foward").click(function() {
 //   console.log("fastfoward")
 // });
-// var moves;
-// moves = ['#start'];
-$("#play_pause").click(function() {
-  $('#start').removeClass('start')
-  $('#white1').addClass('white1')
+var currentMove = 0;
+var totalMoves = 3;
+
+
+$("#next").click(function() {
+  var currentId = "#move_" + currentMove;
+  var nextId = currentId;
+  //makes sure it doesn't go over the total number of moves in this game
+  // "I have another move to make"
+  if (currentMove < totalMoves) {
+    ++currentMove;
+    nextId = "#move_" + currentMove;
+
+    $(currentId).removeClass("showMove");
+    $(currentId).addClass("hideMove");
+
+    $(nextId).removeClass("hideMove");
+    $(nextId).addClass("showMove");
+  }
 });
-$("#white_1").click(function() {
-  $('#start').addClass('start')
-  $('#white1').removeClass('white1')
+
+$("#previous").click(function() {
+  var currentId = "#move_" + currentMove;
+  var previousId = currentId;
+  //makes sure it doesn't go over the total number of moves in this game
+  // "I have another move to make"
+  if (currentMove > 0) {
+    --currentMove;
+    previousId = "#move_" + currentMove;
+
+    $(currentId).removeClass("showMove");
+    $(currentId).addClass("hideMove");
+
+    $(previousId).removeClass("hideMove");
+    $(previousId).addClass("showMove");
+  }
 });
+
+// $("#play_pause").click(function() {
+//   $('#start').removeClass('start')
+//   $('#move_1').addClass('white1')
+// });
+// $("#move_1").click(function() {
+//   $('#start').addClass('start')
+//   $('#move_1').removeClass('white1')
+// });
